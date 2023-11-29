ee8eb83694df0b86d0080f2734fccce26c239d44
Damir Akhmetgaleev <artbox12345@mail.ru>
2023-11-29 22:49:22 +0500

    Save

--- DZ_003/Program.cs
+++ DZ_003/Program.cs
@@ -11,9 +11,9 @@
 // int count = 0;
 // for (int i = 0; i < size; i++)
 //     if (array[i] >= 20 && array[i] <= 90)
-// {
-// count++;
-// }
+//     {
+//         count++;
+//     }
 // Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [20,90]: {count}");
 
 
@@ -32,7 +32,7 @@
 // for (int i = 0; i < size; i++)
 // {
 //     if ((array[i] % 2) == 0)
-//     count++;
+//         count++;
 // }
 // Console.WriteLine($"Количество четных чисел в массиве: {count}");
 
@@ -41,46 +41,23 @@
 // Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 
-// double[]result=new double[10];
-// for(int i=0; i>result.lenght; i++)
-// {
-//     result[i]=Math.Round(new Random().NextDouble()*new Random().Next(1001),2);
-// }
-// Console.WriteLine($"Результат [{string.join("; ", result)}]");
-
 
 int size = 10;
 double[] array = new double[size];
 for (int i = 0; i < size; i++)
 {
-    array[i] = Math.Round(new Random().NextDouble()*new Random().Next(0, 101),2);
+    array[i] = Math.Round(new Random().NextDouble() * new Random().Next(0, 101), 2);
 }
 Console.WriteLine($"[{string.Join(";  ", array)}]");
 double min = 0;
 double max = 0;
 for (double i = 0; i < size; i++)
 {
-    if (min > i) min = i;
-    if (max < i) max = i;
+    if (array[i] > max) max = i;
+    if (array[i] < min) min = i;
 }
-Console.WriteLine($" Максимальный элемент: {max}, Минимальный элемент: {min}");
-Console.WriteLine($"Разница между максимальным и минимальным элементом: {max-min}");
+Console.WriteLine($"Максимальный элемент: {max}, Минимальный элемент: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {max - min}");
 
 
-// Console.WriteLine("Введите размер массива:");
-// int n = Convert.ToInt32(Console.ReadLine()); // Задаём размер массива
-// int [] mass  = new int[n];      // Создаём массив
-// Random rnd = new Random();      // Создание объекта для генерации чисел
-// for (int i = 0; i < n; i++)
-//     { mass[i] = rnd.Next(-50, 50);  // Заполнение рандомами
-//     Console.Write($" {mass[i]} ");
-// }
-// int min = 0;
-// int max = 0;
-// foreach (int i in array)  // Поиск максимального и минимального значения
-// {
-//     if (min > i) min = i;
-//     if (max < i) max = i;
-// }
-// Console.WriteLine($"\n Максимальный элемент: {array}\n Минимальный элемент: {min}");
-// Console.WriteLine($"Разница между максимальным и минимальным элементом: {max-min}");
+
